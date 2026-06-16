a93436f21ce0017a8e136fdeb8af35fe83e880b5
kawizahendrix8-collab <your-email@example.com>
2026-06-16 06:10:48 +0200

    Added an automatic ID createer

--- 02-auth-system-v1/Controller.js
+++ 02-auth-system-v1/Controller.js
@@ -4,6 +4,7 @@ const users = require("./data/users");
 
 exports.signUp = (req, res) => {
   const newUser = {  
+    id: users.length + 1,
     name: req.body.name,
     email: req.body.email,
     password: req.body.password
