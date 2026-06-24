30ebe1d9444e5e72822156a7219258a1d28246d5
kawizahendrix8-collab <your-email@example.com>
2026-06-24 22:55:19 +0200

    Added  post route logic and get route logic. So the server can basically receive and send back notes

---
+++ 03-Notes-System-v1/Controller.js
@@ -0,0 +1,18 @@
+const Note = require("./data.js");
+
+const createNote = async (req, res) => {
+  const newNote = new Note({
+    title: req.body.title,
+    content: req.body.content
+  });
+  
+  await newNote.save();
+  res.send("Note saved!");
+};
+
+const getNotes = async (req, res) => {
+  const allNotes = await Note.find();
+  res.json(allNotes);
+};
+
+module.exports = { createNote, getNotes };
